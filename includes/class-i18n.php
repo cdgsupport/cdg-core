c13bc8ce357617374231cad59750fb71815bd639
Crawford Design Group <Support@crawforddesigngp.com>
2025-12-16 09:27:23 -0600

    Add strict types to load_plugin_textdomain method

--- includes/class-i18n.php
+++ includes/class-i18n.php
@@ -1,13 +1,18 @@
 <?php
+declare(strict_types=1);
+
 /**
  * Define the internationalization functionality.
+ *
+ * @package QuickTools
+ * @since 1.0.0
  */
 class Quick_Tools_i18n {
 
     /**
      * Load the plugin text domain for translation.
      */
-    public function load_plugin_textdomain() {
+    public function load_plugin_textdomain(): void {
         load_plugin_textdomain(
             'quick-tools',
             false,
